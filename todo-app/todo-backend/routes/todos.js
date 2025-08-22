52c2f3b689aee66b69dc89f188252d88ea809e22
Jarkko Tuikka <jarkko.tuikka@gmail.com>
2025-08-22 15:40:46 +0300

    12.1-12.13

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,49 +1,88 @@
-const express = require('express');
-const { Todo } = require('../mongo')
+const express = require("express");
+const { Todo } = require("../mongo");
 const router = express.Router();
+const { getAsync, setAsync } = require("../redis");
+const { get } = require("mongoose");
+
+const addedTodosKey = "added_todos";
+
+async function getAddedTodosCount() {
+  const countString = await getAsync(addedTodosKey);
+  const count = countString ? parseInt(countString) : 0;
+  return count;
+}
 
 /* GET todos listing. */
-router.get('/', async (_, res) => {
-  const todos = await Todo.find({})
+router.get("/", async (_, res) => {
+  const todos = await Todo.find({});
   res.send(todos);
 });
 
 /* POST todo to listing. */
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const todo = await Todo.create({
     text: req.body.text,
-    done: false
-  })
+    done: false,
+  });
+
+  const count = await getAddedTodosCount();
+  const newCount = count + 1;
+  await setAsync(addedTodosKey, newCount);
+
   res.send(todo);
+});
+
+router.get("/statistics", async (_, res) => {
+  const count = await getAddedTodosCount();
+  res.send({
+    added_todos: count,
+  });
 });
 
 const singleRouter = express.Router();
 
 const findByIdMiddleware = async (req, res, next) => {
-  const { id } = req.params
-  req.todo = await Todo.findById(id)
-  if (!req.todo) return res.sendStatus(404)
+  const { id } = req.params;
+  req.todo = await Todo.findById(id);
+  if (!req.todo) return res.sendStatus(404);
 
-  next()
-}
+  next();
+};
 
 /* DELETE todo. */
-singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
+singleRouter.delete("/", async (req, res) => {
+  await req.todo.delete();
   res.sendStatus(200);
 });
 
 /* GET todo. */
-singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+singleRouter.get("/", async (req, res) => {
+  res.send(req.todo);
 });
 
 /* PUT todo. */
-singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+router.put("/:id", findByIdMiddleware, async (req, res) => {
+  try {
+    const updatedTodo = await Todo.findByIdAndUpdate(
+      req.todo._id,
+      {
+        text: req.body.text || req.todo.text,
+        done: req.body.done !== undefined ? req.body.done : req.todo.done,
+      },
+      { new: true }
+    );
+
+    if (!updatedTodo) {
+      return res.sendStatus(404);
+    }
+
+    res.send(updatedTodo);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ error: "Failed to update the todo" });
+  }
 });
 
-router.use('/:id', findByIdMiddleware, singleRouter)
-
+router.use("/:id", findByIdMiddleware, singleRouter);
 
 module.exports = router;
