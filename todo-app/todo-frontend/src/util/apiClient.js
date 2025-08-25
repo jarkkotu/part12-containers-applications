735cf7f89681b7d3b80ad14c1f63a69fe7f94179
Jarkko Tuikka <jarkko.tuikka@gmail.com>
2025-08-25 13:12:26 +0300

    12.20

--- todo-app/todo-frontend/src/util/apiClient.js
+++ todo-app/todo-frontend/src/util/apiClient.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  //baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: "http://localhost:8080/api",
 });
 
 export default apiClient;
